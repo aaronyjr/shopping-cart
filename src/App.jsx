0c93fc233a1a4c85d86096e320a8a8e3997aba61
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-10-03 03:42:51 +0800

    Convert fetched json object to array

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import { Header } from './Header'
-import { Navbar } from './Navbar'
-import { useState,        useEffect } from 'react'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import { Header } from "./Header";
+import { Navbar } from "./Navbar";
+import { useState, useEffect } from "react";
 
 function App() {
-
-  const [products, setProducts] = useState(null)
+  const [products, setProducts] = useState(null);
+  if (products) {
+    console.log(typeof products[0]);
+  }
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-  })
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        const array = data.map((product) => Object.values(product));
+        setProducts(array);
+      })
+      .catch((error) => console.log("Error fetching data", error));
+  }, []);
 
   return (
     <>
-    <Header />
-    <Navbar />
-    <Outlet />
+      <Header />
+      <Navbar />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
