eef5e7523ab5987d5af1d18b0504c3e2f082475f
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-10-02 10:24:08 +0800

    Fetch fakestoreapi data

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import { Header } from './components/Header'
 import { Navbar } from './components/Navbar'
+import { useState,        useEffect } from 'react'
 
 function App() {
+
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+  })
 
   return (
     <>
