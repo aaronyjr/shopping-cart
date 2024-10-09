418575c2cbb5441e7811f255db73c53689892d5a
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-10-10 04:52:38 +0800

    Add size button in ProductDetails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,14 +1,21 @@
 import { useLocation, useOutletContext } from "react-router-dom";
 import styles from "./ProductDetails.module.css";
+import { useState } from "react";
 
 export function ProductDetails() {
+  const [size, setSize] = useState("S");
   const { state } = useLocation();
   const product = state?.product;
   const gender = state?.gender;
   const { cartItemCount, setCartItemCount } = useOutletContext();
+  const productSizes = ["S", "M", "L", "XL"];
 
   const addToCart = () => {
     setCartItemCount(cartItemCount + 1);
+  };
+
+  const selectSize = (size) => {
+    setSize(size);
   };
 
   return (
@@ -21,6 +28,19 @@ export function ProductDetails() {
         <p>{"$" + product.price}</p>
         <br />
         <p>{product.description}</p>
+        <div className={styles.sizeSelector}>
+          <p>Select Size:</p>
+          <div className={styles.sizeOptions}>
+            {productSizes.map((productSize) => (
+              <button 
+              onClick={() => selectSize(productSize)} 
+              key={productSize} 
+              className={size === productSize ? styles.selectedSize : "" }>
+                {productSize}
+              </button>
+            ))}
+          </div>
+        </div>
         <button onClick={addToCart}>Add to cart</button>
       </div>
     </div>
