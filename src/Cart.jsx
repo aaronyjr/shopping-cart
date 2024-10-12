dea9c37cce45e7cc89992990f5a4f8690215d23e
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-10-13 03:45:40 +0800

    Restructure images

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,9 +1,13 @@
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import { useLocation } from "react-router-dom";
 
 export function Cart() {
   const { cartItems, removeItemFromCart, increaseQuantity, decreaseQuantity } =
     useContext(CartContext);
+
+  const { state } = useLocation
+  const product = state?.product
 
   return (
     <div>
@@ -11,6 +15,7 @@ export function Cart() {
       {cartItems.length > 0 ? (
         cartItems.map((item, index) => (
           <div key={index}>
+            <img src={`/img/${gender}/${product.image}`} alt={product.name} />
             <p>{item.product.name} - Size: {item.size}</p>
             <p>Quantity: {item.quantity}</p>
             <button onClick={() => decreaseQuantity(item.product.id, item.size)}>-</button>
