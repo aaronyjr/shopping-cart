919625e6e26dc0a6660140c4476c1afc21cf1f8e
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-10-15 07:38:47 +0800

    Style navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <>
+    <div style={{ margin: '10px', display: 'flex', gap: '10px', justifyContent:'center' }}>
       <Link to="women">Women</Link>
       <Link to="men">Men</Link>
-    </>
+    </div>
   );
 }
