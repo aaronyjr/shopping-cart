33a05d661d8aeeed161529543d6a1ff862b7f051
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-10-15 07:09:41 +0800

    Add logo images

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
-import "@fortawesome/fontawesome-free/css/all.min.css"; // Import Font Awesome
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export function Header() {
   const { cartItems } = useContext(CartContext);
@@ -10,14 +10,16 @@ export function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <Link to="/"><img src="../public/s-logo.svg" alt="Logo" /></Link>
+        <Link to="/">
+          <div className={styles.logoContainer}>
+            <img src="/suit-logo-no-bg.png" alt="Logo" className={styles.logoImage} />
+          </div>
+        </Link>
       </div>
-      <div
-        className={styles.cartButton}
-        data-count={cartItems.length}
-      >
+
+      <div className={styles.cartButton} data-count={cartItems.length}>
         <Link to="/cart">
-          <i className={`fas fa-shopping-cart ${styles.cartIcon}`}></i> {/* Cart icon */}
+          <i className={`fas fa-shopping-cart ${styles.cartIcon}`}></i>
         </Link>
       </div>
     </div>
