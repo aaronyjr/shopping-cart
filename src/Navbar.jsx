4e3b492d3fea1d230c56ac2ca6d96a61cf5ac869
Aaron Yuen <107573923+aaronyjr@users.noreply.github.com>
2024-10-16 01:27:29 +0800

    Fix body width and Navbar naming

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export function Navbar() {
   return (
     <div style={{ fontSize: '22px', margin: '10px', display: 'flex', gap: '30px', justifyContent:'center' }}>
-      <Link to="women">Women</Link>
-      <Link to="men">Men</Link>
+      <Link to="women">Women's Suits</Link>
+      <Link to="men">Men's Suits</Link>
     </div>
   );
 }
